a3de3caac01913e8ad74c287b1f1ead885d166e8
IAmAJedi <jedi@perfectmachines.org>
2017-04-20 13:43:17 -0700

    yuri and ytsearch
    Initial commit includes yuri (the bot itself), and ytsearch (youtube/google example code, hax0red for my needs).
    You should be able to figure out config.py on your own, but here's an example anyway.

---
+++ config.py
@@ -0,0 +1,14 @@
+# Some basic variables used to configure the bot
+server = "irc.bitlust.in" # Server
+port = 6697 # Port
+channel = "#" # Channel
+botnick = "yuri" # Your bots nick
+ircname = 'Yuri Gagarin'
+nsnick = "REDACTED"
+nspw = "REDACTED"
+ytsearchcmd = "ytsearch"
+userdbfile = "userdb.sqlite"
+
+masternick = "jedi"
+masterident = "YOUR IDENT HERE"
+masterhost = "YOUR HOST HERE"
